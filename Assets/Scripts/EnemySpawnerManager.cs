e265ac1eead7dc506ee3abf272d2d9732fde4e8d
SugarKat <46972873+SugarKat@users.noreply.github.com>
2023-02-04 01:22:31 +0200

    Enemy spawning

--- Assets/Scripts/EnemySpawnerManager.cs
+++ Assets/Scripts/EnemySpawnerManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class EnemySpawnerManager : MonoBehaviour
 {
-    public WavePresetSC enemyPrefab;
+    public GameObject playerObject;
+
+    public WavePresetSC wavesPreset;
     [Tooltip("Map size in one direction")]
-    public float mapRadius= 10f;
+    public float mapRadius = 10f;
     public float radiusFromPlayer = 2f;
+
+    public bool spawnenemy = true;
 
     float countdown = 1f;
     int selectedWaveInfo = 0;
@@ -15,32 +19,60 @@ public class EnemySpawnerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!spawnenemy)
+        {
+            return;
+        }
         countdown -= Time.deltaTime;
         if (countdown <= 0f)
         {
-            StartCoroutine(SpawnWave());
-            countdown = 1f;
+            Instantiate(wavesPreset.waves[selectedWaveInfo].Enemies[0], PickPosition(), Quaternion.identity);
+            countdown = wavesPreset.waves[selectedWaveInfo].waveSpawnCooldown;
         }
     }
 
-    IEnumerator SpawnWave()
+    Vector3 PickPosition()
     {
-        /*waveIndex++;
-        PlayerStats.Rounds++;
-
-        for (int i = 0; i < waveIndex; i++)
+        float xPlPos = playerObject.transform.position.x + radiusFromPlayer / 2;
+        float yPlPos = playerObject.transform.position.y + radiusFromPlayer / 2;
+        float x = Random.Range(-mapRadius, mapRadius);
+        float y = Random.Range(-mapRadius, mapRadius);
+        if (x < xPlPos && x > xPlPos - radiusFromPlayer)
         {
-            int enemyID = Random.Range(0, enemyPrefabs.Length);
+            if (y < yPlPos && y > yPlPos - radiusFromPlayer)
+            {                
+                if (xPlPos > mapRadius / 2)
+                {
+                    x -= radiusFromPlayer;
+                }
+                else
+                {
+                    x += radiusFromPlayer;
+                }
+            }
+        }
+        Vector3 pos = new Vector3(x, y);
+        return pos;
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3 upRight = new Vector3(mapRadius, mapRadius);
+        Vector3 lowerLeft = new Vector3(mapRadius, mapRadius);
 
-            SpawnEnemy(enemyID);*/
-            yield return new WaitForSeconds(1.5f);
-            /*enemiesAlive++;
-        }*/
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one * mapRadius * 2);
+
+        //Need to remake this so it shows from player position
+        Gizmos.color = Color.blue;
+        if (playerObject != null)
+        {
+            Gizmos.DrawWireCube(playerObject.transform.position, Vector3.one * radiusFromPlayer);
+        }
     }
 }
