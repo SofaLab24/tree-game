53453982098ff17dc41afa17c179d3cd4f01457a
povilasxd3 <povilas.duoba@unity3d.com>
2023-02-04 01:04:37 +0200

    Added enemy follow AI

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -6,34 +6,36 @@ using static UnityEngine.GraphicsBuffer;
 public class AI : MonoBehaviour
 {
     private Rigidbody2D enemyR;
+    private SpriteRenderer enemySR;
 
     private GameObject player;
     private Transform playerT;
-    // Start is called before the first frame update
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         playerT = player.transform;
         enemyR = GetComponent<Rigidbody2D>();
+        enemySR = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (transform.position.x > playerT.position.x)
         {
-            //target is left
-            playerT.localScale = new Vector2(-1, 1);
+            transform.localScale = new Vector2(-1, 1);           
             enemyR.velocity = new Vector2(-2f, 0f); // -2f galima pakeisti "movement speed"
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y), 2f * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, playerT.position, 2f * Time.deltaTime);
+            if(enemySR.flipX != true)
+            {
+                enemySR.flipX = true;
+            }
         }
         else if (transform.position.x < playerT.position.x)
         {
-            //target is right
-            transform.localScale = new Vector2(1, 1);
+            transform.localScale = new Vector2(-1, 1);
             enemyR.velocity = new Vector2(2f, 0f);
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y), 2f * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, playerT.position, 2f * Time.deltaTime);
         }
     }
 }
