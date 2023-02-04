f660e9bebca0450e8c6763951b15fa284638ff83
SugarKat <46972873+SugarKat@users.noreply.github.com>
2023-02-05 00:54:55 +0200

    Roots damage done

--- Assets/Scripts/RootWave.cs
+++ Assets/Scripts/RootWave.cs
@@ -19,7 +19,16 @@ public class RootWave : MonoBehaviour
     public void SetTarget(Vector3 target)
     {
         dir = (target - transform.position).normalized;
-        float angle = Vector2.SignedAngle(Vector2.right, dir);
+        float angle = Vector2.SignedAngle(Vector2.right, dir) - 90f;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enemy"))
+        {
+            EnemyController ec = collision.GetComponent<EnemyController>();
+            ec.Root(rootTime);
+            ec.takeDamage(damage);
+        }
     }
 }
